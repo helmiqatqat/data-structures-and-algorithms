4de88eaec5977dc7487aaaeabae8900d67fc0c83
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-06-10 20:49:46 +0300

    linked-list added and tested successfully

---
+++ javascript/code-challenges/linked-list/Linked.test.js
@@ -0,0 +1,21 @@
+const Linkedlist = require('./Linked-list');
+
+describe('test linked list', () => {
+  test('creates a linkedlist', () => {
+    const newList = new Linkedlist();
+    expect(Object.keys(newList).length).toEqual(1);
+  });
+  test('adds to the linkedlist', () => {
+    const newList = new Linkedlist();
+    newList.insert(1);
+    newList.insert(2);
+    expect(newList.head.value).toEqual(2);
+    expect(newList.head.next).toEqual({value: 1, next: null});
+  });
+  test('toString gives specific string', () => {
+    const newList = new Linkedlist();
+    newList.insert(1);
+    newList.insert(2);
+    expect(newList.toString()).toEqual(`{2} -> {1} -> NULL`);
+  });
+});
