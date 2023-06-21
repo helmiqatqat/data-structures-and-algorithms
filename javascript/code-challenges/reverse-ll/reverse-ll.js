d83e3525137fc045bf8d181eee5806d7a94f7e87
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-06-22 01:10:49 +0300

    fixed directory issues

--- javascript/code-challenges/reverse-ll/reverse-ll.js
+++ javascript/code-challenges/reverse-ll/reverse-ll.js
@@ -1,16 +1,14 @@
 'use strict';
 
 // Require the linked list implementation
-const LinkedList = require('../../linked-list');
+const LinkedList = require('../linked-list');
 
-
+function reverse(list) {
+  return true;
+}
 /*
   Accept a linked list
   Reveres it
   Return it
 */
-module.exports = function reverse(list) {
-
-  return true;
-
-};
+module.exports = reverse;
